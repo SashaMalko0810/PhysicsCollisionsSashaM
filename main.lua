e515757d6d03b7abbefcdd626039a0f18fc51e09
Sasha Malko <sasha.malko@ocsbstudent.ca>
2018-03-28 16:11:08 -0400

    added more objects

--- main.lua
+++ main.lua
@@ -20,7 +20,7 @@ physics.start()
 -----------------------------------------------------------------------------------------------
 
 --Create the ground
-local ground = display.newImage("Physics/ground.png", 0, 0)
+local ground = display.newImage("Images/ground.png", 0, 0)
 
 --Set the x and y pos
 ground.x = display.contentCenterX
@@ -35,7 +35,7 @@ physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
 -----------------------------------------------------------------------------------------------
 
 --Create a verticle beam
-local beam  = display.newImage("Physics/beam.png", 0, 0)
+local beam  = display.newImage("Images/beam.png", 0, 0)
 
 --Set the x and y pos
 beam.x = display.contentCenterX/5
@@ -49,7 +49,7 @@ beam.height = display.contentHeight/10
 beam:rotate(45)
 
 --send it to the back layer
-beam.toBack()
+beam:toBack()
 
 --Add to physics
 physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
@@ -57,7 +57,7 @@ physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
 ------------------------------------------------------------------------------------------------
 
 --Create bkg
-local bkg = display.newImage("Physics/bkg.png", 0, 0)
+local bkg = display.newImage("Images/bkg.png", 0, 0)
 
 --Set the x and y pos
 bkg.x = display.contentCenterX
@@ -65,7 +65,7 @@ bkg.y = display.contentCenterY
 
 --Change the width to be the same as the screen
 bkg.width = display.contentWidth
-bkg.width = display.contentWidth
+bkg.height = display.contentHeight
 
 --send to back
 bkg:toBack()
@@ -77,7 +77,7 @@ bkg:toBack()
 --create the first ball
 local function firstBall()
 	--creating first ball
-	local ball1 = display.newImage("Physics/super_ball.png", 0, 0)
+	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
 
 	--add to physics
 	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
@@ -88,7 +88,7 @@ end
 --create the second ball
 local function secondBall()
 	--creating first ball
-	local ball2 = display.newImage("Physics/super_ball.png", 0, 0)
+	local ball2 = display.newImage("Images/super_ball.png", 0, 0)
 
 	--adding to physics
 	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
@@ -101,7 +101,7 @@ end
 --create the first ball
 local function thirdBall()
 	--creating first ball
-	local ball3 = display.newImage("Physics/super_ball.png", 0, 0)
+	local ball3 = display.newImage("Images/super_ball.png", 0, 0)
 
 	--add to physics
 	physics.addBody(ball3, {density=1.0, friction=0.5, bounce=0.3, radius=25})
@@ -112,7 +112,7 @@ end
 --create the second ball
 local function fourthBall()
 	--creating first ball
-	local ball4 = display.newImage("Physics/super_ball.png", 0, 0)
+	local ball4 = display.newImage("Images/super_ball.png", 0, 0)
 
 	--adding to physics
 	physics.addBody(ball4, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
@@ -127,3 +127,5 @@ end
 
 timer.performWithDelay(0, firstBall)
 timer.performWithDelay(500, secondBall)
+timer.performWithDelay(1000, firstBall)
+timer.performWithDelay(1500, secondBall)
